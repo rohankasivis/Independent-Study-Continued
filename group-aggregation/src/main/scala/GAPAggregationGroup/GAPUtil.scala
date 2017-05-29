3c7adb1a29778f7b2960408856b7c03b47722743
rohankasivis <rohan.kasivis@gmail.com>
2017-05-29 11:46:43 -0600

    restoreTable almost done

--- group-aggregation/src/main/scala/GAPAggregationGroup/GAPUtil.scala
+++ group-aggregation/src/main/scala/GAPAggregationGroup/GAPUtil.scala
@@ -31,31 +31,14 @@ class GAPUtil[A] (monoid: Monoid[A]) {
 
   // gets the level of the actor
   def getLevel(curr_actor:ActorRef, table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Int = {
-    var minimum:Int = Int.MaxValue
-    for(value <- table.keys)
-    {
-      var curr_min:Int = table.get(value).get._2
-      if(curr_min < minimum)
-        minimum = curr_min
-    }
-    return minimum + 1
+    get_minimum(table) + 1
   }
 
   // gets the actor's parent
   def getParent(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):ActorRef = {
-    var minimum:Int = Int.MaxValue
-    if(table.size == 0 || table.size == 1)
-      return null
     for(value <- table.keys)
     {
-      var curr_min:Int = table.get(value).get._2
-      if(curr_min < minimum)
-        minimum = curr_min
-    }
-
-    for(value <- table.keys)
-    {
-      if(table.get(value).get._2 == minimum)
+      if(table.get(value).get._1 == Par())
         return value
     }
     null
@@ -77,7 +60,6 @@ class GAPUtil[A] (monoid: Monoid[A]) {
       if(table.get(parent).get._1 == Self())
       {
         val addEntry: Tuple3[The_Status, Int, A] = Tuple3(Child(), level, weight)
-        print("here")
         var new_table = table + (updateActor -> addEntry)
         new_table
       }
@@ -103,7 +85,137 @@ class GAPUtil[A] (monoid: Monoid[A]) {
     table   // just return this for now
   }
 
-  def restore_table_invariant() = {
+  def get_minimum(table:Map[ActorRef, Tuple3[The_Status, Int, A]]): Int = {
+    var minimum:Int = Int.MaxValue
+    for(value <- table.keys)
+    {
+      var curr_min:Int = table.get(value).get._2
+      if(curr_min < minimum)
+        minimum = curr_min
+    }
+    minimum
+  }
 
+  // this handles a table with only one row - makes sure that the only type class is self(), and if not, fixes that
+  def handle_single_row_table(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+    for(value <- table.keys)
+    {
+      if(table.get(value).get._1 != Self())
+      {
+        // fix here by making status self
+        val level = table.get(value).get._2
+        val weight = table.get(value).get._3
+        val addEntry: Tuple3[The_Status, Int, A] = Tuple3(Self(), level, weight)
+        var new_table = table + (value -> addEntry)
+        return new_table
+      }
+    }
+    table
+  }
+
+  // a helper function which makes sure the parent has minimum level in the table
+  def parent_min_val(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+    var minimum:Int = get_minimum(table)
+
+    for(value <- table.keys)
+    {
+      if(table.get(value).get._2 == minimum)
+      {
+        // now we are at what should be the parent
+        val the_weight:A = table.get(value).get._3
+        if(table.get(value).get._1 != Par())
+        {
+          // then make this have a status of parent
+          val addEntry: Tuple3[The_Status, Int, A] = Tuple3(Par(), minimum, the_weight)
+          var new_table = table + (value -> addEntry)
+          return new_table
+        }
+      }
+    }
+    table
+  }
+
+  // a helper function which makes sure that the level (minimum + 1) element has status self()
+  def handle_self_level(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]]  = {
+    var minimum:Int = get_minimum(table)
+    var cond_two_satisfied:Boolean = false
+    for(value <- table.keys)
+    {
+      if(table.get(value).get._1 == Self())
+      {
+        val curr_level:Int = table.get(value).get._2
+        if(curr_level == minimum + 1)
+          cond_two_satisfied = true
+      }
+    }
+
+    // if cond_two is not satisfied, find minimum + 1 level in the table and make that self
+    if(!cond_two_satisfied)
+    {
+      for(value <- table.keys)
+      {
+        if(table.get(value).get._2 == minimum + 1)
+        {
+          val the_weight:A = table.get(value).get._3
+          val addEntry: Tuple3[The_Status, Int, A] = Tuple3(Self(), minimum + 1, the_weight)
+          var new_table = table + (value -> addEntry)
+          return new_table
+        }
+      }
+    }
+    table
+  }
+
+  // makes sure only one parent exists, and if more than one does, replace the other one with an appropriate status
+  def confirm_one_parent(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+
+  }
+
+  def confirm_one_self(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+
+  }
+
+  def confirm_no_repetition(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+
+  }
+
+  // the main carry forward function
+  def restore_table_invariant(table:Map[ActorRef, Tuple3[The_Status, Int, A]]):Map[ActorRef, Tuple3[The_Status, Int, A]] = {
+    if(table.size == 1)
+    {
+      // in this case, make sure there is an entry self - if not, then fix this
+      var final_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = handle_single_row_table(table)
+
+      // returns the result of the above call
+      final_table
+    }
+    else if(table.size > 1)
+    {
+      // check the following conditions: each node has only one row, exactly one node has status self,
+      // there exists a parent and it has the minimum level among all entries; parent's level is 1 greater than self
+
+      // first - make sure the entry with minimum value has status parent
+      var first_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = parent_min_val(table)
+
+      // next - make sure that the actor with self has level of minimum + 1
+      var second_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = handle_self_level(first_table)
+
+      // third - make sure only one parent - if not, then convert the other status to peer/child
+      var third_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = confirm_one_parent(second_table)
+
+      // fourth - make sure only one self - if not, then convert the other status to peer/child
+      var fourth_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = confirm_one_self(third_table)
+
+      // fifth - make sure that no node occurs more than once
+      var final_table:Map[ActorRef, Tuple3[The_Status, Int, A]] = confirm_no_repetition(fourth_table)
+
+      // return the last table after all modifications have been made
+      final_table
+    }
+    else
+    {
+      // no need to do anything then - simply return the table
+      table
+    }
   }
 }
